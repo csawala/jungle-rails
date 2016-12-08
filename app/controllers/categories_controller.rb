6e6b90a8ae34386f12adff0cc46756fa30c21964
csawala <chris.sawala@gmail.com>
2016-12-08 16:24:57 +0000

    completed sold-out identifier

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,10 +6,3 @@ class CategoriesController < ApplicationController
   end
 
 end
-
-
-# create_table "categories", force: :cascade do |t|
-#   t.string   "name"
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-# end
