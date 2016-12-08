129e79d038388a32a553d80901922875fc58d2a8
csawala <chris.sawala@gmail.com>
2016-12-08 00:31:16 +0000

    updated controllers to show table info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -68,3 +68,12 @@ class OrdersController < ApplicationController
   end
 
 end
+
+
+# create_table "orders", force: :cascade do |t|
+#   t.integer  "total_cents"
+#   t.datetime "created_at",       null: false
+#   t.datetime "updated_at",       null: false
+#   t.string   "stripe_charge_id"
+#   t.string   "email"
+# end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,3 +9,15 @@ class ProductsController < ApplicationController
   end
 
 end
+
+
+# create_table "products", force: :cascade do |t|
+#   t.string   "name"
+#   t.text     "description"
+#   t.string   "image"
+#   t.integer  "price_cents"
+#   t.integer  "quantity"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+#   t.integer  "category_id"
+# end
