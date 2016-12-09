26766c8cb3c3198991c38d8a58a2f046c30445e2
csawala <chris.sawala@gmail.com>
2016-12-09 00:12:15 +0000

    added user authentication/registration/login/logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
+
+
+# class CreateUsers < ActiveRecord::Migration
+#   def change
+#     create_table :users do |t|
+#       t.string :first_name
+#       t.string :last_name
+#       t.string :email
+#       t.string :password
+
+#       t.timestamps null: false
+#     end
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,13 @@ Rails.application.routes.draw do
     resources :products, :categories, except: [:edit, :update, :show]
   end
 
+  resources :users
+  resources :sessions
+
+  get '/logout' => 'sessions#destroy'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
